b2dcc92aa9731bc38006fa499e8214076afaa9f1
GiuMangiaracina <giulia.mangiaracina95@gmail.com>
2020-09-25 18:22:13 +0200

    .
    .

--- program/spark3/app.sh
+++ program/spark3/app.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-{ time spark-submit --master local[2] --conf spark.hadoop.fs.s3a.endpoint=http://127.0.0.1:8002 script.py 2>1 ;} 2>> time.log
+{ time spark-submit --master local[2] --conf spark.hadoop.fs.s3a.endpoint=http://127.0.0.1:8003 script.py 2>1 ;} 2>> time.log
